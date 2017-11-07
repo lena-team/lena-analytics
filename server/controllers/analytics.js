900942aaf984b72d2638e544ff1de704f71da6d2
Eugene Soo <eugene@soo.sg>
2017-11-06 20:21:39 -0800

    refactor code

--- server/controllers/analytics.js
+++ server/controllers/analytics.js
@@ -48,7 +48,7 @@ const updateEventTypeTime = (eventsArray, eventsData) => {
   return Promise.all(eventPromise);
 };
 
-const updateElasticRefactored = () => {
+const updateElastic = () => {
   const events = [];
   let eventsDataArray;
   let elasticResults;
@@ -66,56 +66,8 @@ const updateElasticRefactored = () => {
     .catch(err => console.log(err));
 };
 
-const updateElastic = () => {
-  const events = [];
-  let finalResults = [];
-  let elasticResults;
-  return eventType.getEventTypes()
-    .then((results) => {
-      const eventPromise = [];
-      results.rows.forEach((eventT) => {
-        if (eventT.endtime) {
-          events.push(eventT.eventname);
-          eventPromise.push(event.findEvent(eventT.eventname, eventT.endtime));
-        }
-      });
-      return Promise.all(eventPromise);
-    })
-    .then((results) => {
-      finalResults = results; // this is the same as EventsDataArray
-      const elasticData = [];
-      results.forEach((eventData) => {
-        for (let i = 0; i < eventData.rows.length; i += 1) {
-          elasticData.push({
-            index: {
-              _index: 'event',
-              _type: eventData.rows[i].eventtype,
-            },
-          });
-          elasticData.push(eventData.rows[i]);
-        }
-      });
-      return elasticClient.bulk({
-        body: elasticData,
-      });
-    })
-    .then((results) => {
-      elasticResults = results;
-      const eventPromise1 = [];
-      events.forEach((event, index) => {
-        eventPromise1.push(eventType.updateEventType(
-          event,
-          finalResults[index].rows[finalResults[index].rows.length - 1].eventcreatedat,
-        ));
-      });
-      return Promise.all(eventPromise1);
-    })
-    .then(() => elasticResults);
-};
-
 module.exports = { updateElastic };
 
 // setInterval(() => {
 // updateElasticRefactored();
 // }, 5000);
-updateElasticRefactored();
